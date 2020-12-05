19683e496bd1db0f2c7dd985685519a5f924afbc
Sven <37796423+sven09@users.noreply.github.com>
2020-12-05 12:01:57 +0100

    if id exist the import will update the items

--- services/utils/content.js
+++ services/utils/content.js
@@ -1,24 +1,33 @@
-const importItemByContentType = (id, item) => {
-  return strapi.query(id).create(item);
-};
+const importItemByContentType = async (targetModel, item) => {
 
-const importSingleType = async (uid, item) => {
-  const existing = await strapi.query(uid).find({});
-  if (existing.length > 0) {
-    return strapi.query(uid).update({
-      id: existing[0].id,
+  // check if item with id exists
+  const existing = await strapi.query(targetModel).find({id: item.id});
+  if (existing.length > 0 && item.id) {
+    return strapi.query(targetModel).update({
+      id: item.id,
     }, item)
   } else {
-    return strapi.query(uid).create(item);
+    return strapi.query(targetModel).create(item);
   }
 };
 
-const findAll = (uid) => {
-  return strapi.query(uid).find({});
+const importSingleType = async (targetModel, item) => {
+  const existing = await strapi.query(targetModel).find({});
+  if (existing.length > 0) {
+    return strapi.query(targetModel).update({
+      id: existing[0].id,
+    }, item)
+  } else {
+    return strapi.query(targetModel).create(item);
+  }
 };
 
-const deleteByIds = (uid, ids) => {
-  return strapi.query(uid).delete({
+const findAll = (targetModel) => {
+  return strapi.query(targetModel).find({});
+};
+
+const deleteByIds = (targetModel, ids) => {
+  return strapi.query(targetModel).delete({
     id_in: ids
   });
 };
